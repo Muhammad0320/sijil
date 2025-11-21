50beabc3e88d562ee61c65dd7ec1e29dc73fc40e
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-11-21 17:18:08 +0100

    chore: add registerformschema

--- log-dashboard/src/lib/definitions.ts
+++ log-dashboard/src/lib/definitions.ts
@@ -15,5 +15,21 @@ export const LoginFormSchema = z.object({
   email: z.email({ error: "Please enter a valid email address." }),
   password: z
     .string()
-    .min(8, { error: "password field must be at least 8 characters long" }),
+    .min(8, { error: "Password must be at least 8 characters long" }),
+});
+
+export const RegisterFormSchema = z.object({
+  name: z
+    .string()
+    .min(2, { error: "Name must be at least 2 characters long" })
+    .trim(),
+  email: z.email({ error: "Please enter a valid email address." }).trim(),
+  password: z
+    .string()
+    .min(8, { error: "password must be at least 8 characters" })
+    .trim()
+    .regex(/[a-zA-Z]/, { error: "Contain at least one letter" })
+    .regex(/[0-9]/, { error: "Contains at least one number" })
+    .regex(/[^a-zA-z0-9]/, { error: "Contains at least one special character" })
+    .trim(),
 });
