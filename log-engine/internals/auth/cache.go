9855e1dee4c29e6f4a97992565ef1511f5ed473d
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-11-29 07:54:08 +0100

    chore: initial commit for pivot plan

---
+++ log-engine/internals/auth/cache.go
@@ -0,0 +1,85 @@
+package auth
+
+import (
+	"context"
+	"crypto/sha256"
+	"log-engine/internals/database"
+	"sync"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	ShardCount = 256
+	TTL        = 5 * time.Minute
+)
+
+type ProjectCacheEntry struct {
+	ProjectID int 
+	Expires int64
+}
+
+// The Room (shard)
+type CasheShard struct {
+	sync.RWMutex
+	items map[string]ProjectCacheEntry
+}
+
+// The Hallway (The manager)
+type AuthCache struct {
+	shards [ShardCount]*CasheShard
+	db *pgxpool.Pool
+}
+
+func NewAuthtCache(db *pgxpool.Pool) *AuthCache {
+	c := &AuthCache{db: db} 
+	for i := 0; i < ShardCount; i++ {
+		c.shards[i] = &CasheShard{
+			items: make(map[string]ProjectCacheEntry),
+		}
+	}
+	return c; 
+}
+
+func (c *AuthCache) getShard(apiKey string) *CasheShard {
+	hash := sha256.Sum256([]byte(apiKey))
+
+	shardIndex := hash[0]
+	return c.shards[shardIndex]
+}
+
+func (c *AuthCache) ValidateAPIKey(ctx context.Context, apiKey, apiSecret string) (int, bool) {
+
+	// Step 1: Check RAM (fast path)
+	shard := c.getShard(apiKey) 
+	
+	shard.RLock()
+	entry, exists := shard.items[apiKey]
+	shard.RUnlock()
+
+	if exists && time.Now().Unix() > entry.Expires {
+		return entry.ProjectID, true
+	}
+
+	// Step 2: Check DB (slow path - Cache Miss)
+	project, err := database.GetProductByApiKey(ctx, c.db, apiKey)
+	if err != nil {
+		return 0, false
+	}
+
+	// Step 3: Check secret (slow math)
+	if !ComparePasswordHash(apiKey, project.ApiSecretHash) {
+		return 0, false
+	}
+
+	// Step 4: Update cache
+	shard.Lock()
+	shard.items[apiKey] = ProjectCacheEntry{
+		ProjectID: project.ID,
+		Expires: time.Now().Add(TTL).Unix(),
+	}
+	shard.Unlock()
+
+	return  project.ID, true
+}
