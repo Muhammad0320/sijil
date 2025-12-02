56ed720289db150d695ee3d641199b5499ae9875
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-02 03:32:08 +0100

    chore: update wal.go

--- log-engine/internals/ingest/wal.go
+++ log-engine/internals/ingest/wal.go
@@ -1,6 +1,7 @@
 package ingest
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log-engine/internals/database"
@@ -47,4 +48,46 @@ func (w *WAL) WriteLog(entry database.LogEntry) error {
 
 func (w *WAL) Close() error {
 	return w.file.Close()
+}
+
+func (w *WAL) Recover() ([]database.LogEntry, error) {
+
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	_, err :=w.file.Seek(0, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	var logs []database.LogEntry
+	scanner := bufio.NewScanner(w.file)
+
+	for scanner.Scan() {
+		var entry database.LogEntry
+		if err := json.Unmarshal(scanner.Bytes(), &entry); err != nil {
+			// In Production we should add corrupt.log to store corrupt logs
+			fmt.Printf("⚠️ WAL Corrupt Line: %v\n", err)
+			continue
+		}
+		logs = append(logs, entry)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return  logs, nil 
+}
+
+func (w *WAL) Clear() error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if err := w.file.Truncate(0); err != nil {
+		return  err
+	}
+
+	_, err := w.file.Seek(0, 0)	
+	return  err
 }
