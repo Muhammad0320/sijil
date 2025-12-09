90123125e992014c54e183226fc04bb7b0e5493d
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-09 07:30:58 +0100

    chore: updated

--- log-dashboard/src/app/(auth)/layout.tsx
+++ log-dashboard/src/app/(auth)/layout.tsx
@@ -4,7 +4,6 @@ import {
   AuthTitle,
 } from "@/components/features/auth/AuthCompoents";
 import { ReactNode } from "react";
-// We'll reuse the styled components, but refactored slightly
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
