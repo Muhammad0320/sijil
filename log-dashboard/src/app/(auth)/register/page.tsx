ea43fb0223fbad91b1ad86c22e64f0ee289bcbd0
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-09 08:12:39 +0100

    chore: updated

--- log-dashboard/src/app/(auth)/register/page.tsx
+++ log-dashboard/src/app/(auth)/register/page.tsx
@@ -1,15 +1,13 @@
-import { AuthLink } from "@/components/features/auth/AuthCompoents";
+import { StyledAuthLink } from "@/components/features/auth/AuthCompoents";
 import RegisterForm from "@/components/features/auth/RegisterForm";
-
-import Link from "next/link";
 
 export default function RegisterPage() {
   return (
     <>
       <RegisterForm />
-      <Link href="/login" passHref legacyBehavior>
-        <AuthLink>Already have an account? Sign in</AuthLink>
-      </Link>
+      <StyledAuthLink href={"/login"}>
+        Already have an account? sign in.
+      </StyledAuthLink>
     </>
   );
 }
