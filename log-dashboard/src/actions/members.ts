89acf67bb6a20391713fbb6b46af8aaef2c804a2
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-12 11:19:10 +0100

    chore: updated

--- log-dashboard/src/actions/members.ts
+++ log-dashboard/src/actions/members.ts
@@ -3,24 +3,49 @@
 import { fetchClient } from "@/lib/client";
 import { getSession } from "@/lib/session";
 import { revalidatePath } from "next/cache";
+import { InviteSchema, InviteState } from "../lib/definitions";
 
 export async function inviteMemberAction(
-  projectId: number,
-  email: string,
-  role: string
-) {
+  prevstate: InviteState,
+  formData: FormData
+): Promise<InviteState> {
   const token = await getSession();
-  if (!token) throw new Error("Unauthorized");
 
-  await fetchClient(
-    `/projects/${projectId}/members`,
-    {
-      method: "POST",
-      body: JSON.stringify({ email, role }),
-    },
-    token
-  );
+  const validation = InviteSchema.safeParse({
+    email: formData.get("email"),
+    role: formData.get("role"),
+    projectId: formData.get("projectId"),
+  });
 
-  revalidatePath("/");
-  return { success: true };
+  if (!validation.success) {
+    return {
+      errors: validation.error.flatten().fieldErrors,
+    };
+  }
+
+  const { email, role, projectId } = validation.data;
+
+  try {
+    await fetchClient(
+      `/projects/${projectId}/members`,
+      { method: "POST", body: JSON.stringify({ email, role }) },
+      token
+    );
+
+    revalidatePath("/dashboard");
+
+    return { success: true, message: `Invited ${email} as ${role}` };
+  } catch (error) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message || "Failed to invite member"],
+        },
+      };
+    }
+
+    return {
+      errors: { _form: ["Failed to invite member"] },
+    };
+  }
 }
