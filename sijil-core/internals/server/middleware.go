913eeb270c95d7496f4d121550d3ab5a1d8340d7
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-18 09:53:15 +0100

    chore: change the entire app from logengine to sijil

--- sijil-core/internals/server/middleware.go
+++ sijil-core/internals/server/middleware.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
@@ -14,8 +15,34 @@ import (
 type IPRateLimiter struct {
 
 	ips map[string]*rate.Limiter
-	
+	mu *sync.RWMutex
+	r rate.Limit
+	b int
 
+}
+
+func NewIPRateLimiter(r rate.Limit, b int) *IPRateLimiter {
+
+	return &IPRateLimiter{
+		ips: make(map[string]*rate.Limiter),
+		mu: &sync.RWMutex{},
+		r: r,
+		b: b,
+	}
+}
+
+func (i *IPRateLimiter) GetLimiter(ip string) *rate.Limiter {
+
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	limiter, exists :=  i.ips[ip]
+	if !exists {
+		limiter := rate.NewLimiter(i.r, i.b)
+		i.ips[ip] = limiter
+	}
+	
+	return limiter
 }
 
 // Control Room Guard
