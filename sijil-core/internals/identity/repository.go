43ba69a6af464ffcaedd3d289c4ba02b9f6b9a2a
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-21 08:27:47 +0100

    chore: update

--- sijil-core/internals/identity/repository.go
+++ sijil-core/internals/identity/repository.go
@@ -7,8 +7,8 @@ import (
 
 type Repository interface {
 	Create(ctx context.Context, u *User) (int, error)
-	GetByEmail(ctc context.Context, email string) (*User, error)
+	GetByEmail(ctx context.Context, email string) (*User, error)
 	GetByID(ctx context.Context, id int) (*User, error)
-	UpdateVerification(ctx context.Context, userID int, isVerified bool) error
+	VerifyUserAccount(ctx context.Context, token string) error
 	SetResetToken(ctx context.Context, email string, token string, expiry time.Time) error
 }
