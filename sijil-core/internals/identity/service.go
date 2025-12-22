d67a4f2cfd36a19227a890d453e14cff337ce236
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2025-12-22 05:03:08 +0100

    chore: update

--- sijil-core/internals/identity/service.go
+++ sijil-core/internals/identity/service.go
@@ -3,7 +3,10 @@ package identity
 import (
 	"context"
 	"errors"
+	"fmt"
 	"sijil-core/internals/auth"
+	"sijil-core/internals/utils"
+	"time"
 )
 
 type Service struct {
@@ -19,18 +22,29 @@ func (s *Service) Register(ctx context.Context, req RegisterRequest) (string, er
 
 	hash, _ := auth.HashPasswod(req.Password)
 
+	rawToken, _ := utils.GenerateRandomString(32)
+	hashedToken := utils.Hashtoken(rawToken)
+	expires := time.Now().Add(24 * time.Hour)
+
 	u := &User{
 		FirstName:    req.FirstName,
 		LastName:     req.LastName,
 		Email:        req.Email,
 		PasswordHash: hash,
-		Plan:         "free",
+
+		Plan:                     "free",
+		VerificationToken:        &hashedToken,
+		VerificationTokenExpires: &expires,
 	}
 
 	id, err := s.repo.Create(ctx, u)
 	if err != nil {
 		return "", err
 	}
+
+	go func(email, token string) {
+		fmt.Printf("📧 [Email Mock] To: %s | Subject: Verify Account | Link: https://sijil.dev/verify?token=%s\n", email, token)
+	}(u.Email, rawToken)
 
 	return auth.CreateJWT(s.jwtSecret, id)
 }
@@ -47,4 +61,32 @@ func (s *Service) Login(ctx context.Context, req LoginRequest) (string, error) {
 	}
 
 	return auth.CreateJWT(s.jwtSecret, user.ID)
+}
+
+func (s *Service) VerifyEmail(ctx context.Context, rawToken string) error {
+	hashedToken := utils.Hashtoken(rawToken)
+	return s.repo.VerifyUserAccount(ctx, hashedToken)
+}
+
+func (s *Service) ForgotPassword(ctx context.Context, email string) error {
+
+	_, err := s.repo.GetByEmail(ctx, email)
+	if err != nil {
+		return nil
+	}
+
+	rawToken, _ := utils.GenerateRandomString(32)
+	hashToken := utils.Hashtoken(rawToken)
+	expiry := time.Now().Add(15 * time.Minute)
+
+	err = s.repo.SetPasswordResetToken(ctx, email, hashToken, expiry)
+	if err != nil {
+		return err
+	}
+
+	go func(email, token string) {
+		fmt.Printf("📧 [Email Mock] To: %s | Subject: Reset Password | Link: https://sijil.dev/reset-password?token=%s\n", email, token)
+	}(email, rawToken)
+
+	return nil
 }
