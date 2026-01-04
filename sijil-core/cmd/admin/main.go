c328ee9daf28ffb768d312675af4ae83d95963b8
Balogun Muhammad Awwal <balogunmuhammadawwal@gmail.com>
2026-01-04 09:05:41 +0100

    chore: updates

--- sijil-core/cmd/admin/main.go
+++ sijil-core/cmd/admin/main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func mian() {
+
+	_ = godotenv.Load()
+
+	action := flag.String("action", "", "Actions to perform: promote | ban | reset-pass | stats")
+	email := flag.String("email", "", "Target user email")
+	planID := flag.Int("paln", 0, "Plan ID (1=Hobby, 2=Pro, 3=Ultra)")
+
+	flag.Parse()
+
+	if *action == "" {
+		printHelp()
+		return
+	}
+
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl != "" {
+
+		dbPass := os.Getenv("DATABASE_PASSWORD")
+		if dbPass != "" {
+			dbUrl = fmt.Sprintf("postgres://postgres:%s@127.0.0.1:5434/log_db?sslmode=disable", dbPass)
+		} else {
+			log.Fatal("DATABASE_URL or DB_PASSWORD is required")
+		}
+	}
+
+	ctx := context.Background()
+	db, err := pgxpool.New(ctx, dbUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+}
+
+func printHelp() {
+	fmt.Println("Sijil Admin CLI tool")
+	fmt.Println("Flags:")
+	flag.PrintDefaults()
+}
